b2fbb497068377d736400dfb44a40202a87e417e
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-11-30 13:04:10 +0800

    第一次提交..fvd23

--- src/main/java/base/HelloWorld.java
+++ src/main/java/base/HelloWorld.java
@@ -7,6 +7,6 @@ public class HelloWorld {
         System.out.println("Test");
         System.out.println("");
         System.out.println("dsdsd");
-        System.out.println("dddddddddd");
+        System.out.println("TangSH");
     }
 }
