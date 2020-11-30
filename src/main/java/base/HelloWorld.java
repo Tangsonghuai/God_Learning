e760a02e1f0a9cc722acd1847556461e06ebd26f
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-11-30 12:50:27 +0800

    第一次提交.

--- src/main/java/base/HelloWorld.java
+++ src/main/java/base/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Test");
         System.out.println("Test");
+        System.out.println("");
     }
 }
