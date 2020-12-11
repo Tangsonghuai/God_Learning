f216aaf6881a50548e582706c0de7db68fe5fe2e
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-11 20:46:29 +0800

    接口

---
+++ src/main/java/class_dir/interface_father.java
@@ -0,0 +1,10 @@
+package class_dir;
+
+/**
+ * God_Learning
+ *
+ * @author tangsonghuai
+ * @date 2020/12/11 20:40
+ */
+public class interface_father {
+}
---
+++ src/main/java/class_dir/interface_son.java
@@ -0,0 +1,10 @@
+package class_dir;
+
+/**
+ * God_Learning
+ *
+ * @author tangsonghuai
+ * @date 2020/12/11 20:46
+ */
+public class interface_son {
+}
