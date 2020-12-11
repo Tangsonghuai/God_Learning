d604e979a99a15cb64a0db2dff90296028ae3eaa
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-11 12:21:21 +0800

    类继承

--- src/main/java/class_dir/student.java
+++ src/main/java/class_dir/student.java
@@ -24,10 +24,13 @@ public class student extends person {
 
 
     public static void main(String[] args) {
+
+//        person person =new student(); // 对象向上转型，作用是对象调用父类的方法的时候，得到的一定是子类覆写过后的方法（当然，前提是子类覆写父类方法）
+
         student student =new student();
         student.setAge(12);                 // 父类 引入 lombok 包，@data注解可省略定义参数 set/get方法
         student.setName("Tony");
-        student.tell("传参");                     //子类可以重用父类的属性与方法，前提是父类修饰属性/方法用的关键字是 public/protect，不可private，private作用域在本类中
+        student.tell("传参");                     //子类可以重载父类的属性与方法（也叫多态），前提是父类修饰属性/方法用的关键字是 public/protect，不可private，private作用域在本类中
         student.tell();//子类
 
         System.out.println(student.name+" 's age "+student.age);
