66e41fedc36070f0ec94b82df7247b29c80fc03f
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-11 22:53:52 +0800

    接口

--- src/main/java/class_dir/interface_father.java
+++ src/main/java/class_dir/interface_father.java
@@ -6,5 +6,8 @@ package class_dir;
  * @author tangsonghuai
  * @date 2020/12/11 20:40
  */
-public class interface_father {
+public interface interface_father {
+    //抽象类中 可以有 全局常量  default方法 static方法
+    public static final  String name="全局常量";
+    public abstract String tell();
 }
--- src/main/java/class_dir/interface_son.java
+++ src/main/java/class_dir/interface_son.java
@@ -6,5 +6,15 @@ package class_dir;
  * @author tangsonghuai
  * @date 2020/12/11 20:46
  */
-public class interface_son {
+public class interface_son implements interface_father {
+    public String tell() {
+        return "必须实现该方法";
+    }
+
+    public static void main(String[] args) {
+        interface_son is = new interface_son();
+        is.tell();
+    }
+
+
 }
