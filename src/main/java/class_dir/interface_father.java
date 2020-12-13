a24a384920518e3119fbd99e10db4002c9c797c1
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-13 10:32:39 +0800

    接口

--- src/main/java/class_dir/interface_father.java
+++ src/main/java/class_dir/interface_father.java
@@ -7,7 +7,7 @@ package class_dir;
  * @date 2020/12/11 20:40
  */
 public interface interface_father {
-    //抽象类中 可以有 全局常量  default方法 static方法
+    //接口类中 可以有 全局常量  default方法 static方法，必须是public
     public static final  String name="全局常量";
     public abstract String tell();
 }
