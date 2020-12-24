20afe217e5f57407d4bade09874b4a55552eaaae
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-24 22:01:58 +0800

    Map集合

--- src/main/java/Collect_demo/hashMap_demo.java
+++ src/main/java/Collect_demo/hashMap_demo.java
@@ -6,16 +6,15 @@ import java.util.Set;
 
 /**
  * God_Learning
- *
  * @author tangsonghuai
  * @date 2020/12/24 21:49
  */
 public class hashMap_demo {
     public static void main(String[] args) {
         HashMap<String,Integer> hashMap =new HashMap<String, Integer>();
-              hashMap.put("One",1);
-              hashMap.put("Two",2);
-              hashMap.put("Three",3);
+        hashMap.put("One",1);
+        hashMap.put("Two",2);
+        hashMap.put("Three",3);
         Set<Map.Entry<String,Integer>> set = hashMap.entrySet();    //entry 实例，可以getKey，GetValue
         for (Map.Entry<String,Integer> entry : set){
             System.out.println(entry.getKey()+"__"+entry.getValue());
