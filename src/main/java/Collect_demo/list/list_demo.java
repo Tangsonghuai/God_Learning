50427f3b2d30fa849bfb73539bed533b1bc570d9
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-24 21:34:28 +0800

    List集合

---
+++ src/main/java/Collect_demo/list/list_demo.java
@@ -0,0 +1,28 @@
+package Collect_demo.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * God_Learning
+ *
+ * @author tangsonghuai
+ * @date 2020/12/24 21:20
+ */
+public class list_demo {
+    public static void main(String[] args) throws NullPointerException {
+        List<String> list =new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        System.out.println(list.contains("a")); //集合中是否含有此元素
+        System.out.println(list.isEmpty());     //集合是否为空
+        System.out.println(list.size());        //集合元素个数
+        System.out.println(list.get(0));        //获取集合第一个元素
+        Object result[] = list.toArray();
+
+        for(Object temp : result){
+            System.out.println(temp+" ");
+        }
+    }
+}
