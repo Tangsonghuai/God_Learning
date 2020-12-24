b7688e37a601054435bbda97e2c2392eb1e7ede8
Tangsonghuai <34538205+Tangsonghuai@users.noreply.github.com>
2020-12-24 20:05:32 +0800

    文件

---
+++ src/main/java/IO/OutputStream_demo.java
@@ -0,0 +1,28 @@
+package IO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * God_Learning
+ *
+ * @author tangsonghuai
+ * @date 2020/12/24 19:58
+ */
+
+/*
+* 文件写入
+* */
+public class OutputStream_demo {
+    public static void main(String[] args) throws Exception {
+        File file = new File("D:"+File.separator+"game"+File.separator+"result.txt");
+        if (!file.getParentFile().exists()){
+            file.getParentFile().mkdir();
+        }
+        FileOutputStream outputStream_demo =new FileOutputStream(file);
+        String str01="hello,Tony!";
+        outputStream_demo.write(str01.getBytes());
+        outputStream_demo.close();
+    }
+}
